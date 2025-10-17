defd76aad3e63e5b1cda409eb8af0c9c131d5bd5
salmamal <salmamedhat637@gmail.com>
2025-10-17 12:08:26 +0300

    added workflow for java proje6t

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -7,6 +7,6 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calc = new Calculator();
-        assertEquals(5, calc.add(2, 3));
+        assertEquals(5, calc.add(1, 3));
     }
 }
