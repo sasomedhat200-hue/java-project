7e3a837beb7ecbe102ac8b3e1a3056615a15f558
salmamal <salmamedhat637@gmail.com>
2025-10-20 21:52:30 +0300

    fix: false addition3

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -7,6 +7,6 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calc = new Calculator();
-        assertEquals(11, calc.add(7, 5));
+        assertEquals(11, calc.add(7, 6));
     }
 }
